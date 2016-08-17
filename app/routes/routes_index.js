37efeb16a0f15e39947e04ff1ab1ebcda94cf9fa
Siddhartha Chowdhury <siddhartha.chowdhury@akademe.in>
2016-08-17 15:56:01 +0530

    Mongo added

--- app/routes/routes_index.js
+++ app/routes/routes_index.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var router = express.Router();
 
+////https://www.airpair.com/javascript/complete-expressjs-nodejs-mongodb-crud-skeleton
 // router.use(function(req, res, next) {
 //   // .. some logic here .. like any other middleware
 //   next();
@@ -14,6 +15,6 @@ router.get('/', function(req, res) {
 
 router.post('/post', function(req, res){
 	res.json( req.body);
-	
+
 });
 module.exports = router;
