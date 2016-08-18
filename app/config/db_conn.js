09ed31d6104cb399bd6827267faa739f4983bcbe
Siddhartha Chowdhury <siddhartha.chowdhury@akademe.in>
2016-08-18 15:08:05 +0530

    Removed some type

--- app/config/db_conn.js
+++ app/config/db_conn.js
@@ -1,18 +1,25 @@
 
 var db_config = {
 	connection : 'mongo',
-	db_name : "wikinf"
+	db_name : "mydb"
 }
+
+//------------------ RESTRICTED [start]---------------------------
+// 	You dont necessarily need to make any changes in this section.
+//	By default we will are using MongoDB. In case you want
+// 	to have some other database, only then you can make 
+//	necessary changes below.
+//							OR
+//	If you have additional information required for establishing
+//	the connection then you can go ahead and make the requied changes
+ 
 var db_conn = {
 	mongo : function(){
 		var mongoose   = require('mongoose');
-
-		// db_name = 'wikinf';					// Enter the database name
-		console.log("reached")
 		mongoose.Promise = global.Promise;
 		mongoose.connect('mongodb://localhost/'+db_config.db_name);
 	}
 }
 
 module.exports = db_conn[db_config.connection];
-		
+//------------------ RESTRICTED [end]---------------------------
--- app/models/user.js
+++ app/models/user.js
@@ -8,17 +8,10 @@ var userSchema = new Schema({
   password: { type: String, required: true },
   admin: Boolean,
   phone: Number,
-  // meta: {
-  //   age: Number,
-  //   website: String
-  // },
   created_at: Date,
   updated_at: Date
 });
 
-// the schema is useless so far
-// we need to create a model using it
 var User = mongoose.model('User', userSchema);
 
-// make this available to our users in our Node applications
 module.exports = User;
